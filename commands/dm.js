4286a95677033268c05e6328d0acae743bbb236e
Parham <parhamlashgari1997@gmail.com>
2020-10-17 20:17:08 +0330

    invite link changed

--- commands/dm.js
+++ commands/dm.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const embed = new Discord.MessageEmbed();
 embed.setTitle('دعوت کردن بات به سرور شخصی')
     .setImage('https://i.pinimg.com/736x/ee/5f/54/ee5f545f99b81b74260342a4c2738ea3.jpg')
-    .addField('لینک دعوت کردن بات به سرور شخصی','https://discord.com/oauth2/authorize?client_id=766247914254893116&scope=bot&permissions=366427263')
+    .addField('لینک دعوت کردن بات به سرور شخصی','https://discord.com/oauth2/authorize?client_id=766247914254893116&scope=bot&permissions=8')
     .addField('چگونه بات را دعوت کنم ؟','برای دعوت کردن بات به سرور کافی است از لینک بالا کمک بگیرید توجه داشته باشید برای دعوت کردن بات حتما باید رنک Moderator داشته باشید')
     .addField('درصورت داشتن هرگونه سوال و یا انتقاد','P4rh4m#4648')
     .addField('among-us-bot@v2.0.0','https://discord.gg/4sNYeU')
